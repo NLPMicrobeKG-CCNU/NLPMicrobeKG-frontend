825528d47c7b2f42aa4601b0ce40ecee9d713694
HCLacids <1246311560@qq.com>
2020-12-01 00:43:46 +0800

    init

---
+++ NLPMicrobeKG/src/pages/test/index.js
@@ -0,0 +1,18 @@
+import './index.css';
+import Graphin, { Utils } from '@antv/graphin';
+import { Toolbar } from '@antv/graphin-components';
+import '@antv/graphin/dist/index.css'; // Graphin CSS
+import '@antv/graphin-components/dist/index.css'; // Graphin 组件 CSS
+
+const Index = (props) =>{
+  const data = Utils.mock(10).circle().graphin();
+  return(
+    <div className="App">
+    <Graphin data={data} layout={{ name: 'concentric' }}>
+      <Toolbar />
+    </Graphin>
+  </div>
+  )
+}
+
+export default Index;
