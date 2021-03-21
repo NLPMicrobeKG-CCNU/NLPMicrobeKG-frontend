0da1678e095e3eefab0d2c22ed0be7f713995369
HCLacids <1246311560@qq.com>
2021-03-21 21:39:03 +0800

    feat:logo

--- NLPMicrobeKG/src/component/header.js
+++ NLPMicrobeKG/src/component/header.js
@@ -1,15 +1,45 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Menu, Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 import { Link } from "react-router-dom";
+import logo from '../assets/schoolLogo.png';
 import "./header.css"
+
+const menu = (mode) => (
+  <Menu>
+    <Menu.Item>
+      <Link to={`/${mode}`} className="link">
+        MicrobeKG
+      </Link>
+    </Menu.Item>
+    <Menu.Item>
+      <Link to={`/${mode}`} className="link">
+        MDepressionKG
+      </Link>
+    </Menu.Item>
+  </Menu>
+);
+
+
 
 const Header = () =>{
     return (
         <header>
         <div className="header-container">
+          <img src="https://www.ccnu.edu.cn/images/footer_logo.png" alt="" className="logo" />
           <div className="header-title">MicrobeKG</div>
           <div className="header-content">
             <Link to ="/home" className="link"><div className="Home margin">Home </div></Link>
-            <Link to ="/explore" className="link"><div className="Explore margin">Explore Microbes </div></Link>
-            <Link to ="/visualization" className="link"><div className="Visualization margin">Visualization</div></Link>
+            <Dropdown overlay={menu('explore')}>
+              <Link to="/explore" className="ant-dropdown-link link margin" onClick={e => e.preventDefault()}>
+                Explore Microbes <DownOutlined />
+              </Link>
+            </Dropdown>
+            <Dropdown overlay={menu("visualization")}>
+              <Link to="/visualization" className="ant-dropdown-link link margin" onClick={e => e.preventDefault()}>
+                Visualization <DownOutlined />
+              </Link>
+            </Dropdown>
             <Link to ="/contact" className="link"><div className="contact margin">Contact us </div></Link>
           </div>
         </div>
